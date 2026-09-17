8b1f6595df2874fcdf274026109601d4c4071b99
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-09-17 22:14:11 +0530

    new commit

---
+++ hello.sh
@@ -0,0 +1,27 @@
+#i/bin/bash
+
+
+echo " hello world"
+
+touch hello.txt
+
+echo "hello gay! " > hello.txt
+
+
+
+git init
+
+git remote add origin  https://github.com/Soumyadeepjana1/demo-shell.git
+
+
+git add .
+
+git status
+git commit -m "new commit"
+
+
+git push origin master
+
+
+
+
